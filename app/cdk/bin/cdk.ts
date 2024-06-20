aca4c0f11ab6312ea6873b3d67f31a637b4d81f2
manu-mishra <manu-mishra>
2024-06-20 22:31:24 +0000

    adding cloud 9 environment test as well

--- app/cdk/bin/cdk.ts
+++ app/cdk/bin/cdk.ts
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ApplicationStack } from '../stacks/application-stack';
-import { UIStack } from '../stacks/ui-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { ApplicationStack } from "../stacks/application-stack";
+import { UIStack } from "../stacks/ui-stack";
+import { Cloud9EnvironmentStack } from "../stacks/cloud9-stack";
 
 const app = new cdk.App();
 
-const appStack=new ApplicationStack(app, 'ApplicationStack', {
-  env: { region: 'us-east-1' }
+const appStack = new ApplicationStack(app, "ApplicationStack", {
+  env: { region: "us-east-1" },
 });
 
-      const uistack = new UIStack(app, 'UIStack', {
-          env: { region: 'us-east-1' }
-      });
-      uistack.addDependency(appStack);
-      
-      app.synth();
+const Cloud9Stack = new Cloud9EnvironmentStack(app, "Cloud9Stack", {
+  env: { region: "us-east-1" },
+});
+const uistack = new UIStack(app, "UIStack", {
+  env: { region: "us-east-1" },
+});
+uistack.addDependency(appStack);
+
+app.synth();
--- app/cdk/stacks/cloud9-stack.ts
+++ app/cdk/stacks/cloud9-stack.ts
@@ -10,27 +10,21 @@ export class Cloud9EnvironmentStack extends cdk.Stack {
     super(scope, id, props);
 
     // Parameters for the GitHub repo
-    const gitRepoUrl = new cdk.CfnParameter(this, 'GitRepoUrl', {
-      type: 'String',
-      description: 'The URL of the GitHub repository to clone',
-    });
+    // const gitRepoUrl = new cdk.CfnParameter(this, 'GitRepoUrl', {
+    //   type: 'String',
+    //   description: 'The URL of the GitHub repository to clone',
+    // });
 
-    const gitBranch = new cdk.CfnParameter(this, 'GitBranch', {
-      type: 'String',
-      description: 'The branch of the GitHub repository to clone',
-      default: 'main',
-    });
-
-    // Create a new VPC
-    const vpc = new ec2.Vpc(this, 'Cloud9Vpc', {
-      maxAzs: 2,
-    });
+    // const gitBranch = new cdk.CfnParameter(this, 'GitBranch', {
+    //   type: 'String',
+    //   description: 'The branch of the GitHub repository to clone',
+    //   default: 'main',
+    // });
 
     // Create a new Cloud9 environment
-    const cloud9Env = new cloud9.Ec2Environment(this, 'Cloud9Env', {
-      vpc: vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      subnetSelection: { subnetType: ec2.SubnetType.PUBLIC },
+    const cloud9Env = new cloud9.CfnEnvironmentEC2(this, 'Cloud9Env', {
+      imageId:"ubuntu-22.04-x86_64",
+      instanceType: "t3.micro",
     });
 
     // Create a policy statement to allow access for all admins
@@ -46,38 +40,38 @@ export class Cloud9EnvironmentStack extends cdk.Stack {
     adminRole.addToPolicy(adminPolicyStatement);
 
     // Create an SSM document to run commands on the Cloud9 instance
-    const ssmDocument = new ssm.CfnDocument(this, 'CloneRepoDocument', {
-      documentType: 'Command',
-      content: {
-        schemaVersion: '2.2',
-        description: 'Clones a GitHub repository into the Cloud9 environment',
-        mainSteps: [
-          {
-            action: 'aws:runShellScript',
-            name: 'cloneRepo',
-            inputs: {
-              runCommand: [
-                `git clone --branch ${gitBranch.valueAsString} ${gitRepoUrl.valueAsString} ~/environment/repo`,
-              ],
-            },
-          },
-        ],
-      },
-    });
+    // const ssmDocument = new ssm.CfnDocument(this, 'CloneRepoDocument', {
+    //   documentType: 'Command',
+    //   content: {
+    //     schemaVersion: '2.2',
+    //     description: 'Clones a GitHub repository into the Cloud9 environment',
+    //     mainSteps: [
+    //       {
+    //         action: 'aws:runShellScript',
+    //         name: 'cloneRepo',
+    //         inputs: {
+    //           runCommand: [
+    //             `git clone --branch ${gitBranch.valueAsString} ${gitRepoUrl.valueAsString} ~/environment/repo`,
+    //           ],
+    //         },
+    //       },
+    //     ],
+    //   },
+    // });
 
     // Grant Cloud9 instance role permission to use the SSM document
-    cloud9Env.node.addDependency(ssmDocument);
+    // cloud9Env.node.addDependency(ssmDocument);
     cloud9Env.node.addDependency(adminRole);
 
-    // Attach the SSM document to the Cloud9 environment
-    const c9Instance = cloud9Env.node.defaultChild as cloud9.CfnEnvironmentEC2;
-    c9Instance.addOverride('Properties.InstanceType', 't3.micro');
-    c9Instance.addOverride('Properties.OwnerArn', adminRole.roleArn);
-    c9Instance.addOverride('Properties.SsmDocuments', [
-      {
-        Name: ssmDocument.ref,
-      },
-    ]);
+    // // Attach the SSM document to the Cloud9 environment
+    // const c9Instance = cloud9Env.node.defaultChild as cloud9.CfnEnvironmentEC2;
+    // c9Instance.addOverride('Properties.InstanceType', 't3.micro');
+    // c9Instance.addOverride('Properties.OwnerArn', adminRole.roleArn);
+    // c9Instance.addOverride('Properties.SsmDocuments', [
+    //   {
+    //     Name: ssmDocument.ref,
+    //   },
+    // ]);
 
     // Grant the Cloud9 environment permissions to execute the SSM document
     const ssmPolicy = new iam.PolicyStatement({
